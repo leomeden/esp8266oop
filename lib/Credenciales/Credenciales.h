9de020f8bb53a70adc59a9241c3a5b784b63aa21
leomeden <leonardomeden@gmail.com>
2023-11-03 02:26:07 -0300

    Se cambio libreria NTP por actualizada de Taranais

---
+++ lib/Credenciales/Credenciales.h
@@ -0,0 +1,9 @@
+#ifndef _CREDENCIALES_H
+#define _CREDENCIALES_H
+
+//Credenciales Wifi
+const char* ssid = "Galaxy A24 78C5";
+const char* pass = "noa12345";
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
-
+//Credenciales y constantes
+#include <Credenciales.h>
+//Clases propias
 #include <WifiConexion.h>
+
 
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 
-const char* ssid = "Galaxy A24 78C5";
-const char* pass = "noa12345";
-
 WifiConexion wifi(ssid, pass);
 
-
+//Hora por NTP
 WiFiUDP ntpUDP;
-
 // You can specify the time server pool and the offset (in seconds, can be
 // changed later with setTimeOffset() ). Additionally you can specify the
 // update interval (in milliseconds, can be changed using setUpdateInterval() ).
@@ -20,17 +19,13 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", -10800, 3600);
 
 void setup(){
   Serial.begin(115200);
-
   wifi.conectar();
-
   timeClient.begin();
 }
 
 void loop() {
   timeClient.update();
-
-  Serial.println(timeClient.getDay() + " " + timeClient.getFormattedTime());
-
+  Serial.println(timeClient.getFormattedDate() + " " + timeClient.getFormattedTime());
   delay(10000);
 }
 
